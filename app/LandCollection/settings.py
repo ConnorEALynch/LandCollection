dc9c89a8d56d76214748eef0ef8ffcb2d8879f6e
unknown <empirepenguins@gmail.com>
2024-03-05 14:47:45 -0500

    dockerfile-dev fixed, small path tweaks

--- app/LandCollection/settings.py
+++ app/LandCollection/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static"  
+    os.path.join(BASE_DIR ,"static")
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join( BASE_DIR , "staticfiles")
 
 
 # Default primary key field type
